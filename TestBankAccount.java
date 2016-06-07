3f4e263b0cf85545eded4dfeea56d02de67c43d3
unknown <jrobinson@WIN-C2IHHT7SA42.aquilent.fundatis.com>
2016-06-07 14:33:06 -0400

    Added Unit Tests

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -1,15 +1,16 @@
 package my.test.bank.account;
 
-
-
 import junit.framework.Assert;
-
 import org.junit.Before;
-
 import org.junit.Test;
 
-
-
 public class TestBankAccount {
+	
+	@Test
+	public void testDebitWithSufficientFunds() {
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(5);
+		Assert.assertEquals(5.0, amount);
+	}
 
 }
